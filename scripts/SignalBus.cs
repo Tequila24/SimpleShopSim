0ec8262834f67b1d576904ebda1f69512f4e5cbc
Tequila24 <ka4xid@gmail.com>
2026-02-06 13:19:01 +0400

    etc

--- scripts/SignalBus.cs
+++ scripts/SignalBus.cs
@@ -7,6 +7,8 @@ public partial class SignalBus : Node
 	public static SignalBus Instance
 	{ private set; get; }
 
+	public static Action<bool> OnShopStateSwitch;
+
 	public static Action<Node3D> OnFurniturePickedUp;
 	public static Action<Node3D> OnFurniturePlaced;
 }	
